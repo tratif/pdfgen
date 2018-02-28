9dc167c80dbf241a0db0b6069ca124d72c7e2f1c
maciej.nowicki <maciej.nowicki@tratif.com>
2018-02-28 16:10:00 +0100

    DocumentBuilder's exceptions refactored.

--- src/main/java/com/tratif/pdfgen/document/DocumentBuilder.java
+++ src/main/java/com/tratif/pdfgen/document/DocumentBuilder.java
@@ -1,13 +1,17 @@
 package com.tratif.pdfgen.document;
 
 import com.tratif.pdfgen.helpers.CommandLineExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 
 public class DocumentBuilder {
 
-    private final String FILE_PREFIX = "pdfgen";
+    private final static Logger LOG = LoggerFactory.getLogger(DocumentBuilder.class);
+    private final String TEMP_FILE_PREFIX = "pdfgen";
 
     private String content;
 
@@ -17,8 +21,8 @@ public class DocumentBuilder {
 
     public byte[] toPdf() {
         try {
-            File html = File.createTempFile(FILE_PREFIX, ".html");
-            File pdf = File.createTempFile(FILE_PREFIX, ".pdf");
+            File html = File.createTempFile(TEMP_FILE_PREFIX, ".html");
+            File pdf = File.createTempFile(TEMP_FILE_PREFIX, ".pdf");
             Files.write(html.toPath(), content.getBytes());
 
             CommandLineExecutor executor = new CommandLineExecutor();
@@ -31,12 +35,20 @@ public class DocumentBuilder {
 
             byte[] bytes = Files.readAllBytes(pdf.toPath());
 
-            html.delete();
-            pdf.delete();
+            if(!html.delete())
+                LOG.warn(html.getPath() + " was not deleted.");
+
+            if(!pdf.delete())
+                LOG.warn(pdf.getPath() + " was not deleted.");
 
             return bytes;
-        } catch(Exception e) {
-            throw new RuntimeException("Something went wrong.");
+
+        } catch (InterruptedException e) {
+            LOG.error("There was problem executing command.");
+            throw new RuntimeException("There was problem executing command.", e);
+        } catch (IOException e) {
+            LOG.error("There was a problem with the file.");
+            throw new RuntimeException("There was a problem with the file.", e);
         }
     }
 }

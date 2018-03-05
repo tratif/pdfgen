be19f31cfe74e4e8ab6efa5e6dad099d81be30a5
maciej.nowicki <maciej.nowicki@tratif.com>
2018-03-05 11:21:29 +0100

    Optimized imports

--- src/test/java/com/tratif/pdfgen/document/DocumentBuilderTest.java
+++ src/test/java/com/tratif/pdfgen/document/DocumentBuilderTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DocumentBuilderTest {
 

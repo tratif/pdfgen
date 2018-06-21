7e6c8591387e9728ae9fdac91af3665b58ac2967
Mateusz Gepert <mateusz.gepert@tratif.com>
2018-06-21 11:08:46 +0200

    added exceptions plus template location

--- src/main/java/com/tratif/pdfgen/document/renderers/html/FreeMarkerHtmlRenderer.java
+++ src/main/java/com/tratif/pdfgen/document/renderers/html/FreeMarkerHtmlRenderer.java
@@ -2,11 +2,13 @@ package com.tratif.pdfgen.document.renderers.html;
 
 import com.tratif.pdfgen.document.docs.HtmlTemplate;
 import com.tratif.pdfgen.document.renderers.HtmlRenderer;
+import freemarker.cache.FileTemplateLoader;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
 
@@ -23,10 +25,34 @@ public class FreeMarkerHtmlRenderer implements HtmlRenderer {
 	@Override
 	public void render(HtmlTemplate page, Writer writer) {
 		try {
+<<<<<<< Updated upstream
 			Template template = configuration.getTemplate(page.asFile().getPath());
+=======
+			//todo make it configurable - freemarker is not able to read templates in custom locations in test scope
+			configuration.setTemplateLoader(new FileTemplateLoader(new File("/opt/tascent/reports/templates/")));
+			Template template = configuration.getTemplate(page.getFilename());
+>>>>>>> Stashed changes
 			template.process(page.getParams(), writer);
 		} catch (IOException | TemplateException e) {
-			throw new RuntimeException("Error while rendering html.");
+			throw new RuntimeException("Error while rendering html.", e);
 		}
 	}
+<<<<<<< Updated upstream
+=======
+
+	//	@Override
+	//	public String render(String htmlTemplate, Map<String, Object> params) {
+	//		throw new RuntimeException();
+	//	}
+	//
+	//	@Override
+	//	public void render(String templateName, Map<String, Object> params, FileWriter fileWriter) {
+	//		try {
+	//			Template template = configuration.getTemplate(templateName);
+	//			template.process(params, fileWriter);
+	//		} catch (IOException | TemplateException e) {
+	//			log.error("[FreeMarker] Could not process template.");
+	//		}
+	//	}
+>>>>>>> Stashed changes
 }

5a129aa2c1e91867941d370a7cfd522cb9ffabb1
Maciej Nowicki <maciej.nowicki@tratif.com>
2018-06-28 09:41:16 +0200

    Added exit code handling.

--- src/main/java/com/tratif/pdfgen/document/renderers/pdf/FilePdfRenderer.java
+++ src/main/java/com/tratif/pdfgen/document/renderers/pdf/FilePdfRenderer.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2018 the original author or authors.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,8 +43,8 @@ public class FilePdfRenderer implements PdfRenderer {
 			logStream(process.getErrorStream());
 			int exitCode = process.waitFor();
 
-			if (exitCode != 0 && exitCode != 1) {
-				throw new RuntimeException("wkhtmltopdf was terminated with exit code " + exitCode);
+			if (exitCode != 0) {
+				handleExitCode(exitCode);
 			}
 
 			return new PdfDocument(new File(destinationFilename));
@@ -57,10 +57,44 @@ public class FilePdfRenderer implements PdfRenderer {
 		}
 	}
 
+	private void handleExitCode(int exitCode) {
+		switch (exitCode) {
+			case 1:
+				log.warn("PDF generated, but some HTTP request may have failed.");
+				break;
+
+			case 2:
+				throw new RuntimeException("PDF failed to render due to unknown error.");
+
+			case 'X':
+				throw new RuntimeException("Could not write PDF: File in use.");
+
+			case 'Y':
+				throw new RuntimeException("Could not write PDF: No write permission.");
+
+			case 'Z':
+				log.warn("PDF generated, but some JavaScript request(s) timed out.");
+				break;
+
+			case 'A':
+				throw new RuntimeException("Invalid arguments provided.");
+
+			case 'B':
+				throw new RuntimeException("Could not find input file(s).");
+
+			case 'C':
+				throw new RuntimeException("Process timed out.");
+
+			default:
+				throw new RuntimeException("Unknown error occurred, although PDF may have been rendered.");
+		}
+	}
+
 	private void logStream(InputStream stream) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
 		String line;
-		while ((line = reader.readLine()) != null)
+		while ((line = reader.readLine()) != null) {
 			log.debug(line);
+		}
 	}
 }
